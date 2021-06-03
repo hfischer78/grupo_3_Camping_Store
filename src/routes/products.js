790ac914d40b0dbdfbb109eb9bc22a3b9144a88d
Seba Calvo <sebacalvo@hotmail.com>
2021-06-03 12:38:00 -0300

    comente algunas rutas de prod

--- src/routes/products.js
+++ src/routes/products.js
@@ -5,10 +5,14 @@ let productsController = require('../controllers/productsController');
 
 routerProducts.get("/productDetail", productsController.detalle);
 
+//rutas para Crear productos
+routerProducts.get("/productCreate", productsController.create); // Esta es para que muestre el formulario de creacion de prod
+//routerProducts.post("/productCreate", productsController.store); //Esta es para ejecutar la logica de creacion de prod
 
-routerProducts.get("/productCreate", productsController.create);
+//rutas para Editar productos
+routerProducts.get("/productEdit", productsController.edit); //Esta es para que muestre el formulario de Editar Prod
+//routerProducts.post("/productEdit", productsController.upload); // ESta es para que ejectue y guarde cambios de la edicion de prod
 
-routerProducts.get("/productEdit", productsController.edit);
 
 
 module.exports = routerProducts;
