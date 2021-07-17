5527b18ff91c4e12ec716d3c4187da2e98fb0a36
hfischer78 <horacio_fischer@hotmail.comm>
2021-07-17 16:22:34 -0300

    actualizacion de usuarios

--- src/middlewares/multerMiddleware.js
+++ src/middlewares/multerMiddleware.js
@@ -16,17 +16,5 @@ return multer({ storage });
  }
 
 
-// const storage = multer.diskStorage({
-//  	destination: (req, file, cb) => {
-// 	cb(null, './public/img/users');
-//  	},
-// 	filename: (req, file, cb) => {
-// 	let fileName = `${Date.now()}_img${path.extname(file.originalname)}`;
-// 	cb(null, fileName);
-//  	}
-// })
-
-//const uploadFile = multer({ storage });
-
 module.exports = multerFunction;
 
