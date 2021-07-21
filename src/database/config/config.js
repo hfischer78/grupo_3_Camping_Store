485a2eeb8e5348b8dacf87f2418ac4a818547d12
hfischer78 <horacio_fischer@hotmail.comm>
2021-07-21 11:52:57 -0300

    Instalo Sequelize

---
+++ src/database/config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": "",
+    "database": "movies_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "port": "3307"
+  },
+  "test": {
+    "username": "root",
+    "password": "",
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "port": "3307"
+  },
+  "production": {
+    "username": "root",
+    "password": "",
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "port": "3307"
+  }
+}
