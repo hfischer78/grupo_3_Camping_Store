6e2bd08a159242f209f3604ef0c57a4a1ebf7b4e
hfischer78 <horacio_fischer@hotmail.comm>
2021-07-25 13:05:28 -0300

    realizamos ajustes en middleware UserLogged

--- src/middlewares/userLoggedMiddleware.js
+++ src/middlewares/userLoggedMiddleware.js
@@ -1,77 +1,43 @@
-// const User = require('../models/User');
-//const User = require('../database/models/U/ser');
+const db = require('../database/models');
+const sequelize = db.sequelize;
+const { Op, Association } = require("sequelize");
+const moment = require('moment');
 
+const User = db.User;
 
 
 function userLoggedMiddleware(req, res, next) {
+	res.locals.isLogged = false;
 
-
-	const db = require('../database/models');
-	const sequelize = db.sequelize;
-	const { Op, Association } = require("sequelize");
-	const moment = require('moment');
 	
-	const User = db.User;
+	let emailInCookie = req.cookies.userEmail;
+	let userFromCookie 
 
-	//res.locals.isLogged = false;
- 	let emailInCookie = req.cookies.userEmail;
- 	//let userFromCookie = User.findByField('email', emailInCookie);
+	//	let userFromCookie = User.findByField('email', emailInCookie);
 
-	 // INICIO DE COPIADO
+// voy a intentar validar con un iff: si esta el cookie userEmail definido entonces busco en la DB, sino sigue el codigo.
 
-db.Users.findOne({where: { email: emailInCookie} })
-        .then(users => {
-            // variable con el dato del mail para validacion
-	
-			res.locals.isLogged = false;
-			
-			let userFromCookie = users
-			
-		// 	userToLogin = users
-       
-        //      if(userToLogin){
-        //      let correctPassword = bcryptjs.compareSync(req.body.password, userToLogin.password);
-        //      if(correctPassword){
-        //        delete userToLogin.password
-        //          req.session.userLogged = userToLogin;
-        //          if (req.body.remember_user){
-        //              res.cookie("userEmail",req.body.email,{maxAge: (1000*60)})
-        //         }
-        //        return res.redirect("/users/profile")
-        //      }
-        //      res.render(path.resolve(__dirname,"../views/users/login.ejs"), {
-        //          errors: {
-        //              email: {
-        //                  msg: 'Credenciales inválidas'
-        //              }
-        //          }
-        //      });
-        //  }
-        //  res.render(path.resolve(__dirname,"../views/users/login.ejs"), {
-        //      errors: {
-        //          email: {
-        //              msg: 'No estás registrado!'
-        //          }
-        //      }
-        //  })
-     
-        }); // cierre del then!
+	if (emailInCookie) {
+		db.Users.findOne({where: { email: emailInCookie} })
+		.then(users1 => {
+		// variable con el dato del mail para validacion
+		userFromCookie = users
+   		}); // cierre del then!
+	}
+
+///
+
+	if (userFromCookie) {
+		req.session.userLogged = userFromCookie;
+	}
+
+	if (req.session.userLogged) {
+		res.locals.isLogged = true;
+		res.locals.userLogged = req.session.userLogged;
+	}
 
 
-
-
-	 //// FIN DE COPIADO
-
- 	if (userFromCookie) {
- 		req.session.userLogged = userFromCookie;
- 	}
-
- 	if (req.session.userLogged) {
- 		res.locals.isLogged = true;
- 		res.locals.userLogged = req.session.userLogged;
- 	}
-
- 	next();
- }
+	next();
+}
 
 module.exports = userLoggedMiddleware;
