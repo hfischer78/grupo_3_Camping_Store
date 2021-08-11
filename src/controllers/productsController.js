fcb9362692ded87ba4e2b8cb1854adcea615337d
hfischer78 <horacio_fischer@hotmail.comm>
2021-08-11 20:19:16 -0300

    Resolvemos problemas en el metodo store de creacion de productos

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -86,21 +86,18 @@ categoryList: function (req, res) {
     // Create -  Method to store
     store: (req, res) => {
        
-        db.Products.create({
-            name: req.body.name,
-            description: req.body.description,
-            price: req.body.price,
-            discount: req.body.discount,
-            // category: req.body.category,
-            color_id: req.body.color,
-            size_id: req.body.size,
+        let productToCreate={
+            ...req.body,
             image: req.file != undefined ? req.file.filename : null,
-        })  
-
-        res.redirect("/");
-    },
-
-    // vista de edición
+            }
+ 
+        db.Products.create(productToCreate)
+         
+           res.redirect("/");
+     },
+ 
+     // vista de edición
+ 
     edit: (req, res) => {
         db.Products.findByPk(req.params.id,{
             include: [
