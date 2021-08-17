c51180c3579e2e57ce35f702daf125124a77e764
hfischer78 <horacio_fischer@hotmail.comm>
2021-08-17 12:33:26 -0300

    ajustamos contradoladores y vistas para crear y editar productos, incluyendo opciones de colores, talles y categorias

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -16,6 +16,10 @@ const { Op, Association } = require("sequelize");
 const moment = require('moment');
 const {validationResult} = require("express-validator");
 const Product = db.Product;
+const Size = db.Size;
+const Color = db.Color;
+const Category = db.Category;
+const Category_Product = db.Category_Product;
 
 
 let controllerProducts = {
@@ -75,7 +79,22 @@ let controllerProducts = {
 
 
     create: function (req, res) {
-            res.render("./products/productCreate", { toThousand });
+         
+           db.Sizes.findAll()
+                                      
+           .then(sizes => {
+            
+                db.Colors.findAll()
+                                    
+                .then(colors => {
+                    db.Categories.findAll()
+                    .then(categories => {
+
+                        res.render("./products/productCreate", {sizes, colors, categories, toThousand });          
+                    })    
+
+            })
+        })  
         
     },
 
@@ -89,28 +108,50 @@ let controllerProducts = {
        
 
         let productToCreate={
-           // ...req.body,
            name: req.body.name,
            description: req.body.description,
            price: req.body.price,
            discount: req.body.discount,
-           //category: req.body.category,
            color_id: req.body.color,
            size_id: req.body.size,
            image: req.file != undefined ? req.file.filename : null,
             }
  
          db.Products.create(productToCreate)
-         
-            res.redirect("/");
+        
+            // accedemos a la tabla de productos para tomar al id del producto recien creado, para luego ingresarlo a tablas.
+            db.Products.findAll(
+            {
+                order: [
+                    ['id', 'DESC'],
+                ],
+                limit: 1
+            })
 
-           
-
-     },
+                .then(AProduct => {
+                    // recorremos las categorias del body y las incluimos en un array para luego ingresar los datos a la DV
+                     let categorias = req.body.Categorias
+                        categorias.forEach (function(id,i) {
+                            categoryToCreate = {  
+                                product_id: AProduct[0].id,
+                                category_id: categorias[i]
+                                
+                             }     
+                              // cargamos los datos en la DB
+                             db.Category_Products.create(categoryToCreate)
+                             res.redirect("/");
+                        }) // cerramos for each
+            
+                }) // cerramos then.
+   
+        }, // cerramos método del controlador.
  
      // vista de edición
  
     edit: (req, res) => {
+       
+        //db.Products.findByPk(req.params.id)
+
         db.Products.findByPk(req.params.id,{
             include: [
             {association: 'colors'},
@@ -119,10 +160,39 @@ let controllerProducts = {
             ]
         })
 
+
         .then(productToEdit => {
-           //res.send(productDetail)
-            res.render('../views/products/productEdit', {productToEdit, toThousand})                
-        })
+        
+            db.Sizes.findAll()
+            .then(sizes => {
+         
+                 db.Colors.findAll()
+                .then(colors => {
+                   
+                    db.Categories.findAll()
+                    .then(categories => {
+
+                        res.render('../views/products/productEdit', {productToEdit,sizes,colors, categories, toThousand})
+                    })
+
+                })                
+            })    
+        })    
+       
+         // desde aca estaba
+        // db.Products.findByPk(req.params.id,{
+        //     include: [
+        //     {association: 'colors'},
+        //     {association:'sizes'},
+        //     {association:'categories'}
+        //     ]
+        // })
+
+        // .then(productToEdit => {
+        //    //res.send(productDetail)
+        //     res.render('../views/products/productEdit', {productToEdit, toThousand})                
+        // })
+
     },
 
     // actualizacion de productos.
