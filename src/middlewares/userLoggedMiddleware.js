086fd632f180e149e71081de98cea1cd26a315eb
Seba Calvo <sebacalvo@hotmail.com>
2021-08-25 20:27:02 -0300

    botton de editar para que lo haga el admin

--- src/middlewares/userLoggedMiddleware.js
+++ src/middlewares/userLoggedMiddleware.js
@@ -6,41 +6,34 @@ const moment = require('moment');
 const User = db.User;
 
 
-async function userLoggedMiddleware(req, res, next) {
+function userLoggedMiddleware(req, res, next) {
 	res.locals.isLogged = false;
 
-	
+	if (req.cookies.userEmail) {
 	let emailInCookie = req.cookies.userEmail;
-	let userFromCookie 
+	let userFromCookie;
 
 	//	let userFromCookie = User.findByField('email', emailInCookie);
 
 // voy a intentar validar con un iff: si esta el cookie userEmail definido entonces busco en la DB, sino sigue el codigo.
+	
+	db.Users.findOne({where: { email: emailInCookie} })
+		.then(user => {
+				delete user.password
+				req.session.userLogged = user; })
+		.catch (e => console.log(e))
+		}
 
-	if (emailInCookie) {
-		db.Users.findOne({where: { email: emailInCookie} })
-		.then(users => {
-		// variable con el dato del mail para validacion
-
-await	(userFromCookie = users)
-
-
-   		}); // cierre del then!
-	}
-
-///
-
-
-	if (userFromCookie) {
-		req.session.userLogged = userFromCookie;
+   	
 
 		if (req.session.userLogged) {
 			res.locals.isLogged = true;
 			res.locals.userLogged = req.session.userLogged;
 		}
+	next();
+		}
 	
-		console.log("ACA",req.session)
-	}
+	
 
 
 
@@ -54,8 +47,5 @@ await	(userFromCookie = users)
   
 ///
 
-
-	next();
-}
 
 module.exports = userLoggedMiddleware;
